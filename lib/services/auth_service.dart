cf83bf0eeec32e56e1e1f63b69b41139020ed149
MikedaGodhack <mikebcool762@gmail.com>
2025-08-14 11:44:03 -0500

    Create auth_service.dart

---
+++ lib/services/auth_service.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Sign in anonymously
+  Future<User?> signInAnonymously() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      return result.user;
+    } catch (e) {
+      print('Error signing in anonymously: $e');
+      return null;
+    }
+  }
+
+  // Sign out
+  Future<void> signOut() async {
+    try {
+      await _auth.signOut();
+    } catch (e) {
+      print('Error signing out: $e');
+    }
+  }
+
+  // Auth change user stream
+  Stream<User?> get user {
+    return _auth.authStateChanges();
+  }
+}
